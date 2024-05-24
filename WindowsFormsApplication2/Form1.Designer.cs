53ed37ec296d7a93f9c59e9fb693f2adf3f5e302
garycor260176 <ipolnikov@gmail.com>
2024-05-24 10:16:26 +0300

    commit

--- WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -29,26 +29,57 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(35, 21);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 29);
+            this.button1.Size = new System.Drawing.Size(163, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(945, 580);
+            this.listBox1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.645161F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.35484F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(951, 620);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(951, 620);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +87,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -13,12 +13,26 @@ namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
-        public struct settings
+        public struct vtt_settings
         {
             public String address;
             public int size;
             public string login;
             public string pwd;
+        };
+        public struct msql_settings
+        {
+            public String server;
+            public string port;
+            public string login;
+            public string pwd;
+            public string dbname;
+        };
+
+        public struct settings
+        {
+            public vtt_settings vtt;
+            public msql_settings mysql;
         };
 
         settings ini;
@@ -28,55 +42,107 @@ namespace WindowsFormsApplication2
             InitializeComponent();
         }
 
-        private settings readSettings() //чтение настройки из какого-нить файла
+        private settings readSettings() //чтение настройки из какого-нить файла (xml, ini или что-то еще)
         {
             settings ret;
-            ret.address = "http://api.vtt.ru:8048/Portal.svc";
-            ret.size = 20;
-            ret.login = "am-458";
-            ret.pwd = "fcv34xvysd";
+            ret.vtt.address = "http://api.vtt.ru:8048/Portal.svc";
+            ret.vtt.size = 20;
+            ret.vtt.login = "am-458";
+            ret.vtt.pwd = "fcv34xvysd";
+
+            ret.mysql.server = "192.168.1.40";
+            ret.mysql.port = "3306";
+            ret.mysql.login = "root";
+            ret.mysql.pwd = "iobroker";
+            ret.mysql.dbname = "vtt";
+
             return ret;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
+
             ini = readSettings();
+            String connStr = 
+                "server=" + ini.mysql.server + ";" + 
+                "user=" + ini.mysql.login + ";" + 
+                "database=" + ini.mysql.dbname + ";" + 
+                "port=" + ini.mysql.port + ";" + 
+                "password=" + ini.mysql.pwd + ";" 
+                ;
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+
             int from = 0;
             vtt.PortalServiceClient client = new vtt.PortalServiceClient();
-            client.Endpoint.Address = new System.ServiceModel.EndpointAddress(ini.address);
-           
+            client.Endpoint.Address = new System.ServiceModel.EndpointAddress(ini.vtt.address);
             while (true)
             {
+                int to = from + ini.vtt.size - 1;
                 try
                 {
-                    vtt.ItemPortionDto items = client.GetItemPortion(ini.login, ini.pwd, from, from + ini.size - 1);
-                    if(items.Items.Length == 0) break;
-                    from = from + ini.size;
-                    SaveTodatabase(items);
+                    vtt.ItemPortionDto items = client.GetItemPortion(ini.vtt.login, ini.vtt.pwd, from, to);
+                    SaveTodatabase(conn, items);
+                    if (items.Items.Length < ini.vtt.size) 
+                        break;
                 }
                 catch (System.ServiceModel.FaultException e1)
-                { 
+                {
+                    listBox1.Items.Add(from.ToString() + " - " + to.ToString() + ": " + e1.Message);
+
                     //e1.Message; куда-нить в лог
-                    break;
                 }
+                from = from + ini.vtt.size;
             }
             client.Close();
+            conn.Close();
         }
 
-        private void SaveTodatabase(vtt.ItemPortionDto items)
+        private void insertItem(MySqlConnection conn, vtt.ItemDto item)
+        {
+            MySqlCommand cmd = new MySqlCommand("vtt_ins", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("aid", item.Id);
+            cmd.Parameters.AddWithValue("aName", item.Name);
+            cmd.Parameters.AddWithValue("aBrand", item.Brand);
+            cmd.Parameters.AddWithValue("aDescription", item.Description);
+            cmd.Parameters.AddWithValue("aGroup", item.Group);
+            cmd.Parameters.AddWithValue("aRootGroup", item.RootGroup);
+            cmd.Parameters.AddWithValue("aAvailableQuantity", item.AvailableQuantity);
+            cmd.Parameters.AddWithValue("aTransitQuantity", item.TransitQuantity);
+            cmd.Parameters.AddWithValue("aPrice", item.Price);
+            cmd.Parameters.AddWithValue("aPriceRetail", item.PriceRetail);
+            cmd.Parameters.AddWithValue("aWidth", item.Width);
+            cmd.Parameters.AddWithValue("aHeight", item.Height);
+            cmd.Parameters.AddWithValue("aDepth", item.Depth);
+            cmd.Parameters.AddWithValue("aWeight", item.Weight);
+            cmd.Parameters.AddWithValue("aPhotoUrl", item.PhotoUrl);
+            cmd.Parameters.AddWithValue("aOriginalNumber", item.OriginalNumber);
+            cmd.Parameters.AddWithValue("aVendor", item.Vendor);
+            cmd.Parameters.AddWithValue("aCompatibility", item.Compatibility);
+            cmd.Parameters.AddWithValue("aBarcode", item.Barcode);
+            cmd.Parameters.AddWithValue("aMainOfficeQuantity", item.MainOfficeQuantity);
+            cmd.Parameters.AddWithValue("aNumberInPackage", item.NumberInPackage);
+            cmd.Parameters.AddWithValue("aColorName", item.ColorName);
+            cmd.Parameters.AddWithValue("aTransitDate", item.TransitDate);
+            cmd.Parameters.AddWithValue("aItemLifeTime", item.ItemLifeTime);
+            cmd.Parameters.AddWithValue("aNameAlias", item.NameAlias);
+            cmd.ExecuteNonQuery();
+        }
+
+        private void SaveTodatabase(MySqlConnection conn, vtt.ItemPortionDto items)
         {
             for (int i = 0; i < items.Items.Length; i++)
             {
-                //select id
-                //    from табличка 
-                //    where id = items.Items[i].Id
-                //if(запись есть) {
-                    //Update табличка set все поля кроме id
-                    //    where id = items.Items[i].Id
-                //} else {
-                    //делаем Insert
-                //}
+                insertItem(conn, items.Items[i]);
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
